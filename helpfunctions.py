ad09e153b0fecb66c7e3431f35294a7cce282543
ertam98 <etamm@kth.se>
2023-02-07 11:37:50 +0100

    Added function combine_lists

--- helpfunctions.py
+++ helpfunctions.py
@@ -3,3 +3,10 @@ import sys
 def streamprinter(text):
     sys.stdout.write(text)
     sys.stdout.flush()
+
+def combine_lists(list1, list2):
+    """Code from https://stackoverflow.com/a/1675380
+    Takes two lists and returns a set with all elements."""
+    s = set(list1)
+    s.update(list2)
+    return s
