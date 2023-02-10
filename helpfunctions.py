64a1f27c495b5756d60a0efc95c1b2c79eaf1b8a
ertam98 <etamm@kth.se>
2023-02-10 13:36:26 +0100

    Added geometric mean and generalised combine_lists

--- helpfunctions.py
+++ helpfunctions.py
@@ -4,9 +4,18 @@ def streamprinter(text):
     sys.stdout.write(text)
     sys.stdout.flush()
 
-def combine_lists(list1, list2):
-    """Code from https://stackoverflow.com/a/1675380
-    Takes two lists and returns a set with all elements."""
-    s = set(list1)
-    s.update(list2)
+def combine_lists(*lists):
+    """Adapted from https://stackoverflow.com/a/1675380
+    Takes lists and returns a set with all elements."""
+    s = set(lists[0])
+    for mylist in lists[1:]:
+        s.update(mylist)
     return s
+
+def geometric_mean(mylist):
+    n = len(mylist)
+    result = 1
+    for element in mylist:
+        result *= element
+
+    return result**(1/n)
