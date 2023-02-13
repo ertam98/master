ae519ae9fd1234a3264737a436c16fb261e6f8cb
ertam98 <etamm@kth.se>
2023-02-13 16:59:20 +0100

    Changed export data

--- optimizer.py
+++ optimizer.py
@@ -4,11 +4,12 @@ import csv
 
 def main():
     instances = list()
-    with open('benchmark-v2-error-2.txt', 'r') as file:
+    with open('benchmark-v2-error.txt', 'r') as file:
         for line in file:
-            instances.append(line[0:-8] + '.ptf.gz')
+            instances.append(line[0:-8] + '.task.gz')
 
-    header = ['Instance', 
+    header = ['Instance',
+              'Response code',
               'Problem status',
               'Solution status',
               'Time',
@@ -16,8 +17,8 @@ def main():
               'Primal norm',
               'Dual norm']
 
-    #createcsv('output_exp.stat', header)
-    #createcsv('output_gm.stat', header)
+    createcsv('output_exp.stat', header)
+    createcsv('output_gm.stat', header)
 
     for instance in instances:
         solve_export('benchmark_exp', instance, 'exp')
@@ -33,7 +34,7 @@ def solve_export(folder, instance, model):
         # task.set_Stream(mosek.streamtype.log, streamprinter)
 
         task.readdataformat(folder + '/' + instance,
-                            mosek.dataformat.ptf,
+                            mosek.dataformat.task,
                             mosek.compresstype.gzip)
 
         task.putdouparam(mosek.dparam.optimizer_max_time, 10.0)
@@ -41,24 +42,31 @@ def solve_export(folder, instance, model):
         rescode = task.optimize()
         task.updatesolutioninfo(mosek.soltype.itr)
 
-        if rescode == mosek.rescode.ok:
-            print('Solved %s for %s' %(instance, model))
-            data = [instance,
-                    task.getintinf(mosek.iinfitem.sol_itr_prosta),
-                    # task.getprosta(mosek.soltype.itr),
-                    task.getintinf(mosek.iinfitem.sol_itr_solsta),
-                    # task.getsolsta(mosek.soltype.itr),
-                    task.getdouinf(mosek.dinfitem.optimizer_time),
-                    task.getintinf(mosek.iinfitem.intpnt_iter),
-                    task.getdouinf(mosek.dinfitem.sol_itr_nrm_xx),
-                    task.getdouinf(mosek.dinfitem.sol_itr_nrm_y)]
+        print('%s for %s terminated with response code: %s' %(instance, model, rescode))
 
-            with open('output_%s.stat' %(model), 'a') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow(data)
-        elif rescode == mosek.rescode.trm_max_time:
-            print('%s for %s terminated due to max time' %(instance, model))
-        else:
-            print('Unexpected error in %s for %s: %s' %(instance, model, rescode))
-            
-main()
+        #if rescode == mosek.rescode.ok:
+            #print('Solved %s for %s' %(instance, model))
+        data = [instance,
+                rescode,
+                task.getintinf(mosek.iinfitem.sol_itr_prosta),
+                # task.getprosta(mosek.soltype.itr),
+                task.getintinf(mosek.iinfitem.sol_itr_solsta),
+                # task.getsolsta(mosek.soltype.itr),
+                task.getdouinf(mosek.dinfitem.optimizer_time),
+                task.getintinf(mosek.iinfitem.intpnt_iter),
+                task.getdouinf(mosek.dinfitem.sol_itr_nrm_xx),
+                task.getdouinf(mosek.dinfitem.sol_itr_nrm_y)]
+        #elif rescode == mosek.rescode.trm_max_time:
+        #    data = [instance,
+        #            ]
+
+        with open('output_%s.stat' %(model), 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
+        #elif rescode == mosek.rescode.trm_max_time:
+        #    print('%s for %s terminated due to max time' %(instance, model))
+        #else:
+        #    print('Unexpected error in %s for %s: %s' %(instance, model, rescode))
+
+if __name__ == '__main__':     
+    main()
