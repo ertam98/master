f65a170ac6be2930fe7be2101c0089da4c4f4d91
ertam98 <etamm@kth.se>
2023-02-14 15:19:57 +0100

    Solve on primal form added

--- optimizer.py
+++ optimizer.py
@@ -38,35 +38,25 @@ def solve_export(folder, instance, model):
                             mosek.compresstype.gzip)
 
         task.putdouparam(mosek.dparam.optimizer_max_time, 10.0)
-        # turn off dualiser?
+        task.putintparam(mosek.iparam.intpnt_solve_form,
+                         mosek.solveform.primal) # solve on primal form
         rescode = task.optimize()
         task.updatesolutioninfo(mosek.soltype.itr)
 
         print('%s for %s terminated with response code: %s' %(instance, model, rescode))
 
-        #if rescode == mosek.rescode.ok:
-            #print('Solved %s for %s' %(instance, model))
         data = [instance,
                 rescode,
                 task.getintinf(mosek.iinfitem.sol_itr_prosta),
-                # task.getprosta(mosek.soltype.itr),
                 task.getintinf(mosek.iinfitem.sol_itr_solsta),
-                # task.getsolsta(mosek.soltype.itr),
                 task.getdouinf(mosek.dinfitem.optimizer_time),
                 task.getintinf(mosek.iinfitem.intpnt_iter),
                 task.getdouinf(mosek.dinfitem.sol_itr_nrm_xx),
                 task.getdouinf(mosek.dinfitem.sol_itr_nrm_y)]
-        #elif rescode == mosek.rescode.trm_max_time:
-        #    data = [instance,
-        #            ]
 
         with open('output_%s.stat' %(model), 'a') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(data)
-        #elif rescode == mosek.rescode.trm_max_time:
-        #    print('%s for %s terminated due to max time' %(instance, model))
-        #else:
-        #    print('Unexpected error in %s for %s: %s' %(instance, model, rescode))
 
 if __name__ == '__main__':     
     main()
