58c6ab6cd5e4c7807884baedc562fd82ae88f5d6
ertam98 <etamm@kth.se>
2023-02-14 15:20:15 +0100

    Changed to object oriented

--- calculate_stats.py
+++ calculate_stats.py
@@ -5,83 +5,96 @@ from helpfunctions import geometric_mean
 
 def main():
     print('---GEOMETRIC MEAN CONE---')
-    gm_times = print_stats('output_gm.stat')
+    GMplot = MyPlotter()
+    GMplot.getdata('output_gm.stat')
+    print(GMplot)
 
     print('---EXPONENTIAL CONE---')
-    exp_times = print_stats('output_exp.stat')
+    Expplot = MyPlotter()
+    Expplot.getdata('output_exp.stat')
+    print(Expplot)
 
-    max_gm = max(gm_times.values())
-    max_exp = max(exp_times.values())
-    maxtime = max(max_gm, max_exp)
-    
-    solved_both = {'gm': list(), 'exp': list()}
-    solved_gm = {'gm': list(), 'exp': list()}
-    solved_exp = {'gm': list(), 'exp': list()}
-    for key in gm_times.keys():
-        if key in exp_times.keys():
-            solved_both['gm'].append(gm_times[key])
-            solved_both['exp'].append(exp_times[key])
-        else:
-            solved_gm['gm'].append(gm_times[key])
-            solved_gm['exp'].append(10)
-    for key in exp_times.keys():
-        if key not in gm_times.keys():
-            solved_exp['gm'].append(10)
-            solved_exp['exp'].append(exp_times[key])
+    GMplot.exportTimePlot(Expplot, 'Geometric Mean cone',
+                         'Exponential cone', 'scatter.png')
 
-    fig, ax = plt.subplots()
-    ax.scatter(solved_both['gm'], solved_both['exp'], c = 'tab:blue')
-    ax.scatter(solved_gm['gm'], solved_gm['exp'], c = 'tab:red')
-    ax.scatter(solved_exp['gm'], solved_exp['exp'], c = 'tab:green')
-    ax.set_xlabel('Geometric mean cone')
-    ax.set_ylabel('Exponential cone')
-    line = np.arange(0, maxtime, 0.01)
-    ax.plot(line, line)
-    plt.savefig('scatter.png')
+class MyPlotter:
+    def __init__(self):
+        self.filename = str()
+        self.nSolved = 0 # number of problems solved correctly
+        self.nMaxtime = 0 # number of problems timed out
+        self.nPrimIll = 0
+        self.nDualIll = 0
+        self.nPriminf = 0
+        self.nDualinf = 0
+        self.nUnknown = 0
+        self.times = dict()
+        self.iterations = dict()
 
-def print_stats(filename):
-    nSolved = 0 # number of problems solved correctly
-    nMaxtime = 0
-    nPrimIll = 0
-    nDualIll = 0
-    nPriminf = 0
-    nDualinf = 0
-    nUnknown = 0
-    times = dict()
+    def getdata(self, filename):
+        with open(filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                # prim and dual feasible and optimal
+                if (int(row['Problem status']) == 1 and
+                    int(row['Solution status']) == 1):
+                    self.nSolved += 1
+                    self.times[row['Instance']] = float(row['Time'])
+                    self.iterations[row['Instance']] = int(row['Iterations'])
+                    # times.append(float(row['Time']))
+                elif int(row['Solution status']) == 5:
+                    self.nPriminf += 1
+                elif int(row['Solution status']) == 6:
+                    self.nDualinf += 1
+                elif int(row['Solution status']) == 7:
+                    self.nPrimIll += 1
+                elif int(row['Solution status']) == 8:
+                    self.nDualIll += 1
+                elif row['Response code'] == 'rescode.trm_max_time':
+                    self.nMaxtime += 1
+                elif (int(row['Problem status']) == 0 and
+                    int(row['Solution status']) == 0):
+                    self.nUnknown += 1
 
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+    def __str__(self):
+        string = 'Problems solved correctly: %d\n' %(self.nSolved)
+        string += 'Geometic mean of time: %.6f\n' %(geometric_mean(self.times.values()))
+        string += 'Problems timed out: %d\n' %(self.nMaxtime)
+        string += 'Primal infeasible: %d\n' %(self.nPriminf)
+        string += 'Dual infeasible: %d\n' %(self.nDualinf)
+        string += 'Primal illposed: %d\n' %(self.nPrimIll)
+        string += 'Dual illposed: %d\n' %(self.nDualIll)
+        string += 'Unknown status: %d' %(self.nUnknown)
+        return string
 
-        for row in reader:
-            # prim and dual feasible and optimal
-            if (int(row['Problem status']) == 1 and
-                int(row['Solution status']) == 1):
-                nSolved += 1
-                times[row['Instance']] = float(row['Time'])
-                # times.append(float(row['Time']))
-            elif int(row['Solution status']) == 5:
-                nPriminf += 1
-            elif int(row['Solution status']) == 6:
-                nDualinf += 1
-            elif int(row['Solution status']) == 7:
-                nPrimIll += 1
-            elif int(row['Solution status']) == 8:
-                nDualIll += 1
-            elif row['Response code'] == 'rescode.trm_max_time':
-                nMaxtime += 1
-            elif (int(row['Problem status']) == 0 and
-                int(row['Solution status']) == 0):
-                nUnknown += 1
+    def exportTimePlot(self, other, selflabel, otherlabel, filename):
+        max_self = max(self.times.values())
+        max_other = max(other.times.values())
+        max_time = max(max_self, max_other)
 
-    print('Problems solved correctly: %d' %(nSolved))
-    print('Geometic mean of time: %.6f' %(geometric_mean(times.values())))
-    print('Problems timed out: %d' %(nMaxtime))
-    print('Primal infeasible: %d' %(nPriminf))
-    print('Dual infeasible: %d' %(nDualinf))
-    print('Primal illposed: %d' %(nPrimIll))
-    print('Dual illposed: %d' %(nDualIll))
-    print('Unknown status: %d' %(nUnknown))
+        solved_both = {'self': list(), 'other': list()}
+        solved_self = {'self': list(), 'other': list()}
+        solved_other = {'self': list(), 'other': list()}
+        for key in self.times.keys():
+            if key in other.times.keys():
+                solved_both['self'].append(self.times[key])
+                solved_both['other'].append(other.times[key])
+            else:
+                solved_self['self'].append(self.times[key])
+                solved_self['other'].append(max_time)
+        for key in other.times.keys():
+            if key not in self.times.keys():
+                solved_other['self'].append(max_time)
+                solved_other['other'].append(other.times[key])
 
-    return times
+        _, ax = plt.subplots()
+        ax.scatter(solved_both['self'], solved_both['other'], c = 'tab:blue')
+        ax.scatter(solved_self['self'], solved_self['other'], c = 'tab:red')
+        ax.scatter(solved_other['self'], solved_other['other'], c = 'tab:green')
+        ax.set_xlabel(selflabel)
+        ax.set_ylabel(otherlabel)
+        line = np.arange(0, max_time, 0.01)
+        ax.plot(line, line)
+        plt.savefig(filename)
 
-main()
+if __name__ == '__main__':
+    main()
