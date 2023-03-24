0e137d0bdd4bf890397e34e0a95c39cb3512f886
ertam98 <etamm@kth.se>
2023-03-24 13:53:00 +0100

    Added restrictToOptimalFace method

--- MyTask.py
+++ MyTask.py
@@ -789,3 +789,19 @@ class MyTask(mosek.Task):
         expdomain = self.appendprimalexpconedomain()
         for cone in range(self.p):
             self.appendacc(expdomain, range(3*cone, 3*cone+3), None)
+
+    def restrictToOptimalFace(self):
+        _, _, skc, skx, _, xc, xx, _, slc, suc, slx, sux, _, _ = self.getsolutionnew(mosek.soltype.bas)
+
+        rx = [slx[j] - sux[j] for j in range(self.getnumvar())]
+        rc = [slc[i] - suc[i] for i in range(self.getnumcon())]
+
+        # Find non-basic non-zero reduced costs
+        fixvars = [j for j, e in enumerate(rx) if abs(e) > 1e-6 and skx[j] != mosek.stakey.bas]
+        fixcons = [i for i, e in enumerate(rc) if abs(e) > 1e-6 and skc[i] != mosek.stakey.bas]
+
+        for var in fixvars:
+            self.putvarbound(var, mosek.boundkey.fx, xx[var], xx[var])
+
+        for con in fixcons:
+            self.putconbound(con, mosek.boundkey.fx, xc[con], xc[con])
