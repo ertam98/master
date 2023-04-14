2f0a87d0b4cd13dd17d9124cd66d615883a6f847
ertam98 <etamm@kth.se>
2023-04-14 12:38:44 +0200

    Minor edit

--- rewriter_presolve.py
+++ rewriter_presolve.py
@@ -3,11 +3,11 @@ from MyTask import MyTask
 from timeit import default_timer as timer
 
 def main():
-    with open('benchmark-v2.txt', 'r') as file:
+    with open('benchmark-v2-small.txt', 'r') as file:
         for line in file:
             instance = line.replace('.mps.gz\n', '')
 
-            rewriter_presolve('benchmark', instance, '.mps.gz', 'benchmark_presolved')
+            rewriter_presolve('benchmark_optimalface_org_presolve', instance, '.task.gz', 'benchmark_optimalface_org_presolve_presolved')
 
 def rewriter_presolve(importfolder, instance, filetype, exportfolder):
     with MyTask() as task:
@@ -18,13 +18,13 @@ def rewriter_presolve(importfolder, instance, filetype, exportfolder):
             start = timer()
             task.presolve()
             end = timer()
-            with open('timing_presolve.stat', 'a') as file:
+            with open('timing_org_presolve_optimalface.stat', 'a') as file:
                 file.write('%s, %f\n' %(instance, end-start))
             print('Finished presolve for %s in %f sek' %(instance, end-start))
 
             task.writedata(exportfolder + "/" + instance + '.task.gz')
         except Exception:
-            with open('timing_presolve.stat', 'a') as file:
+            with open('timing_org_presolve_optimalface.stat', 'a') as file:
                 file.write('%s, %s\n' %(instance, 'infeasible'))
             print(instance, 'infeasible by presolve')
 
